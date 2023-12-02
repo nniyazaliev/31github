9062da5c350b12bdb19e94bc9634f467403467c3
nniyazaliev <niyazaliev.dev@gmail.com>
2023-12-02 20:57:38 +0600

    day_3 variables

--- bin/github31.dart
+++ bin/github31.dart
@@ -1,6 +1,15 @@
 import 'package:github31/github31.dart' as github31;
 
 void main(List<String> arguments) {
-  print('Hello world!');
-  print('Hello world!');
+  int zero = 0;
+  int one = 1;
+  int three = 3;
+
+  double two = 2.0;
+  bool _true = true;
+
+  print('H$three$one$one$zero w${zero}r${one}d $two $_true');
 }
+
+
+// H3110 w0r1d 2.0 true
