0c15eb91c4e4f39afa09ce352b9b235d797d83ab
nniyazaliev <niyazaliev.dev@gmail.com>
2023-12-04 21:25:17 +0600

    If_else_prodolzhenie

--- bin/IfElse.dart
+++ bin/IfElse.dart
@@ -1,13 +1,25 @@
+import 'dart:io';
+
 void ifElse() {
-  String a = 'Вау';
-  String b = 'Вау';
-  String sameA = a;
+  // String a = 'Вау';
+  // String b = 'Вау';
+  // String sameA = a;
 
-  bool r1 = a == b;
-  bool r2 = a == b;
-  bool r3 = identical(a, sameA);
+  // bool r1 = a == b;
+  // bool r2 = a == b;
+  // bool r3 = identical(a, sameA);
 
-  print(r1);
-  print(r2);
-  print(r3);
+  // print(r1);
+  // print(r2);
+  // print(r3);
+
+  int a = 10;
+  int b = -20;
+  int slagaemoe = a + b;
+
+  if (slagaemoe > 0) {
+    print('Rezultat polozhitelnyi i on raven = $slagaemoe');
+  } else {
+    print('Rezultat otricatelnyi i on raven = $slagaemoe');
+  }
 }
