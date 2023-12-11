f470f32a3c9976618462580f379c163e05a9c6fd
nniyazaliev <niyazaliev.dev@gmail.com>
2023-12-11 22:52:41 +0600

    Function, List

--- bin/github31.dart
+++ bin/github31.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 import 'IfElse.dart'; // Подключите ваши другие файлы
 import 'for.dart';
+import 'students.dart';
 
 List<String> fizzBuzz(int n) {
   List<String> list = [];
@@ -19,25 +20,35 @@ List<String> fizzBuzz(int n) {
 }
 
 void main(List<String> arguments) {
-  int zero = 0;
-  int one = 1;
-  int three = 3;
+  // int zero = 0;
+  // int one = 1;
+  // int three = 3;
 
-  double two = 2.0;
-  bool _true = true;
+  // double two = 2.0;
+  // bool _true = true;
 
-  print('H$three$one$one$zero w${zero}r${one}d $two $_true');
-  print('');
+  // print('H$three$one$one$zero w${zero}r${one}d $two $_true');
+  // print('');
 
-  ifElse();
-  loop();
+  // ifElse();
+  // loop();
 
-  List<int> numbers = [1, 2, 3];
-  int length = numbers[2];
-  List<String> chars = List<String>.filled(length, '');
-  chars[numbers.length - 1] = 'y';
-  print("Done!");
+  // List<int> numbers = [1, 2, 3];
+  // int length = numbers[2];
+  // List<String> chars = List<String>.filled(length, '');
+  // chars[numbers.length - 1] = 'y';
+  // print("Done!");
 
-  List<String> result = fizzBuzz(15);
-  print(result);
+  // List<String> result = fizzBuzz(15);
+  // print(result);
+
+  List<Student> students = [
+    Student("Morgan", "Freeman"),
+    Student("Brad", "Pitt"),
+    Student("Kevin", "Spacey"),
+  ];
+
+  for (Student student in students) {
+    student.printFullName();
+  }
 }
---
+++ bin/students.dart
@@ -0,0 +1,10 @@
+class Student {
+  String firstName;
+  String lastName;
+
+  Student(this.firstName, this.lastName);
+
+  void printFullName() {
+    print('$firstName $lastName');
+  }
+}
